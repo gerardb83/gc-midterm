1c00a8e17bcfa275f18d6efd9fce495fece9e014
gerardb83 <gbreitenbeck@gmail.com>
2019-07-15 11:06:38 -0400

    Changed name to David

--- src/Midterm.java
+++ src/Midterm.java
@@ -3,7 +3,7 @@ public class Midterm {
 	public static void main(String[] args) {
 
 		System.out
-				.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and "
+				.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named David and "
 						+ "his trusty band of ninja monkeys.");
 		System.out.println("The trusty band of ninja monkeys were called The Lin Kuei and they loved to eat potatoes.");
 		;
